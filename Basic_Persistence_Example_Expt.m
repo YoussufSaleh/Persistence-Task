2c748bdc6e067b96ad826a26455674f18c2943d3
YoussufSaleh <42232362+YoussufSaleh@users.noreply.github.com>
2020-03-05 06:57:40 +0000

    Add files via upload

---
+++ Basic_Persistence_Example_Expt.m
@@ -0,0 +1,157 @@
+function [result ] = Basic_Eyetracker_Example_Expt(params)
+% Example script for an eye tracking task. 
+% Basic Prosaccades to left or right, 
+% indicated as the disc that changes colour.
+% 
+% As an example, also shows a visual cue at fixation 
+% and plays a sound at the end.
+% 
+% Sanjay Manohar 2017
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ex: experiment parameters
+
+% trial structure
+ex.type            = 'Eyelink example';  % put your experiment name here.
+ex.practiceTrials  = 10;            
+ex.useEyelink      = 1;             % if 0, just use keyboard
+ex.skipScreenCheck = 0;             % for temperamental screens, put 1.
+ex.displayNumber   = 0;             % for multiple monitors
+
+ex.blockLen        = 48;            % trials per block 
+ex.blocks          = 2;             % number of blocks
+
+% Trial variables are combined to make a factorial design.
+% This means that blockLen must be a multiple of the total 
+% number of factors - in this case 2 possible targetPos, and 3 possible foreperiods:
+ex.trialVariables.targetPos     = 1;         % go left or right 
+ex.trialVariables.impulsefactor = [2 3 4]; % possible forepreiod (seconds)
+ex.blockVariables.blockType     = 1;             % between-block variables (none)
+
+% display params 
+ex.targetLocations  = [ 1 0 ] * 450;       % left or right
+ex.bgColour         = [  0   0   0];   % background colour (RGB)
+ex.fgColour         = [255 255 255];   % text colour 
+ex.targetColour     = [128 128 128];   % disc colour
+ex.targetSize       = 70;              % disc radius (px)
+ex.ITI              = 0.500;           % seconds before trial
+
+% saccade params (millisec, pixels)
+ex.fixationTolerance= 240;         % acceptable distance from target (px)
+ex.saccadeTimeout   = 2;           % terminate trial if no saccade in this time (sec)
+ex.saccadeMinSize   = 40;          % saccades smaller than this are ignored (px)
+ex.saccadeTargetSize= 240;         % radius of saccade landing points considered to be within target (px)
+
+% sound files get automatically read into audioplayers, scr.soundPlayer{}
+ex.soundFiles = { '../../media/ping.wav' 
+                  '../../media/REGISTER2.wav' };
+
+% image files get automatically read into psychtoolbox textures, scr.imageTexture()
+ex.imageFiles = { 'Fractal001.png'
+                  'Fractal002.png' };
+
+%%%%%%%%%%%% RUN EXPERIMENT %%%%%%%%%%%%%
+if ~exist('params') params=struct(); end;  % override settings from input parameter?
+result = RunExperiment( @doTrial, ex, params);
+%%%%%%%%%%% END OF EXPERIMENT %%%%%%%%%%%
+
+return
+
+
+function tr=doTrial(scr, el, ex, tr)
+    % This function is run once for each trial in the experiment. 
+    % combine trial-wise and experiment-wise parameters.
+    pa = combineStruct(ex, tr);  
+    
+    % calculate target location on screen, using target position index
+    tr.targetLocation = scr.centre+pa.targetLocations(pa.targetPos,:);
+        
+    tr.key=[]; tr.R=pa.R_INCOMPLETE; % start with trial being incomplete
+
+    % inter-trial display - crosshair - wait until fixated
+    Screen('FillRect',scr.w, pa.fgColour, scr.xhairCoords(:,:,1)'); 
+    Screen('Flip', scr.w);
+    tr=LogEvent(pa, el, tr,'startITI'); % log event at start
+    if(pa.useEyelink)                   % wait for fixation 
+      r=WaitForFixation(el,scr.centre, pa.fixationTolerance,1000*pa.ITI);
+    else
+      KbWait;
+    end
+    
+    % cue image, & foreperiod
+    drawRings(scr,pa);                  
+	src  = [0 0 scr.imageSize{i}]; % source rectangle (px)
+	tpos = scr.centre;             % location of centre of image on screen
+    drc  = [tpos tpos] + ...       % destination rectangle 
+	       [-0.5 -0.5 0.5 0.5].*[scr.imageSize{1} scr.imageSize{1}];
+    Screen('DrawTexture',scr.w, scr.imageTexture(1), src, drc); % draw image
+    Screen('Flip', scr.w);
+    tr=LogEvent(pa, el, tr,'startcue');  
+	
+    if(pa.useEyelink)                    % ensure fixating for the foreperiod
+      r=WaitForFixation(el,scr.centre, pa.fixationTolerance,1000*pa.foreperiod);
+    else
+      KbWait;
+    end
+    [z z kcode]=KbCheck;
+    if kcode(pa.exitkey) tr.R=pa.R_ESCAPE; tr.key=find(kcode);end;  % exit if escape pressed
+
+    % clear all saccades made so far from buffer: we only want to collect
+    % saccades from now on, when deciding if the target has been hit.
+    if(pa.useEyelink) flushEyelinkQueue(el); end;  
+    
+    % reveal the target
+    drawRings(scr,pa);
+    drawRing(scr,pa,pa.targetPos,pa.targetColour);
+    Screen('Flip',scr.w);
+    tr=LogEvent(pa, el, tr,'starttarget');
+    
+    % wait for response
+    if(pa.useEyelink)             % If using eyelink
+      reached=0;saccade=[];       % have the eyes reached the target yet?
+      while ~reached              % if not,
+        % wait for next saccade (or timeout)
+        r=WaitForSaccade3(el, pa.saccadeTimeout, pa.saccadeMinSize);
+        [z z kcode]=KbCheck; tr.key=find(kcode);   % check for keys too
+        if ~any(kcode) & length(r)>2               % if a saccade:
+          saccade = [saccade;r];                   % log saccade
+          % we have reached if the distance of the endpoint to the target
+          % is less than saccadeTargetSize
+          reached = norm(r(2:3)-tr.targetLocation) < pa.saccadeTargetSize;
+        elseif any(kcode)                          % if a key, log it and end.
+          reached=1; tr.key=find(kcode);
+        end;
+      end
+      tr.endpoint = r;
+      tr.saccade=saccade;                          % store the saccade coordinates
+    else  % if not using eyelink, use mouse cursor
+      ShowCursor
+      kcode=0; b=0; while (b(1)==0) && ~any(kcode) % wait for mouse click
+        [x,y,b]=GetMouse;
+        [z z kcode]=KbCheck;
+        tr.key = find(kcode);
+      end;
+      tr.endpoint=[x,y,b];
+      HideCursor;
+    end;
+    if(any(tr.key==pa.exitkey)) tr.R=pa.R_ESCAPE; end;   % exit if escape
+    tr=LogEvent(pa, el, tr, 'saccadeaccepted');  % log that the saccade was accepted
+    tr.rt=tr.saccadeaccepted-tr.starttarget;     % calculate RT from events    
+    if tr.R==pa.R_INCOMPLETE                     % flag the trial as complete.
+        tr.R   = tr.rt;
+    end;
+	
+    play(scr.soundPlayer{1});                    % play sound number 1
+
+
+function drawRings(scr, ex, fillColour)
+    cmd='FrameOval'; col=ex.fgColour; sz=ex.targetSize;
+    if exist('fillColour') cmd='FillOval'; col=fillColour; sz=ex.targetSize; end;
+    for i=1:size(ex.targetLocations,1)
+      c=ex.targetLocations(i,:) + scr.centre;
+      Screen(cmd, scr.w, col, [c-sz c+sz]);
+    end;
+    
+function drawRing(scr,ex,i,fillColour)
+    c = ex.targetLocations(i,:) + scr.centre;
+    Screen('FillOval', scr.w, fillColour, [c-ex.targetSize c+ex.targetSize]);
---
+++ Kb_Task_YS.m
@@ -0,0 +1,112 @@
+
+function KbDemo
+
+KbName('UnifyKeyNames');
+
+
+%% Run KbCheck script
+KbDemoPart1;
+
+return
+
+%% Part 1sc
+function KbDemoPart1
+
+
+% Clear the workspace and the screen
+sca;
+close all;
+clearvars;
+% Here we call some default settings for setting up Psychtoolbox
+PsychDefaultSetup(2);
+% ensure you bypass mac bugs
+Screen('Preference','SkipSyncTests',2)
+% How many screens are there
+screens = Screen('Screens');
+% use the external one if more than one
+ex.displayNumber = min(screens);
+% define black
+black = BlackIndex(ex.displayNumber);
+% Open up a window
+[window windowRect] = PsychImaging('OpenWindow',ex.displayNumber,black);
+
+% image files get automatically read into psychtoolbox textures, scr.imageTexture()
+ex.imageFiles = { '50_pence.jpg'
+                  '20_pence.jpg' };
+while KbCheck; end % Wait until all keys are released.
+
+
+% Displays the key number when the user presses a key.
+counter = 0;
+fprintf('Press any key as many times as you can for 30 seconds');
+fprintf('Press the escape key to exit');
+escapeKey = KbName('ESCAPE');
+startSecs = GetSecs;
+while 1
+  %DRAW CURRENT BAR
+  % start with frame in the centre of the screen which is red.
+  % what is the size of the onscreen window in pixels?
+  [screenXpixels, screenYpixels] = Screen('WindowSize',window);
+  % where is the centre of the screen?
+  [xCenter, yCenter] = RectCenter(windowRect);
+  % define a rectangle based on its co-ordinates
+  baseRect = [0 0 300 600];
+  % centre this rectangle in the middle of the screen
+  RedCentre = CenterRectOnPointd(baseRect,xCenter+450,yCenter);
+  fifty_pence=imread(ex.imageFiles{2});
+  RectColor = [1 0 0];
+
+  
+
+  % set up counter to calculate total number of presses
+  % Check the state of the keyboard.
+  [ keyIsDown, timeSecs, keyCode] = KbCheck;
+  % if Key is down then counter = counter + 1
+  
+  keyCode = find(keyCode, 1);
+  % If the user is pressing a key, then display its code number and name.
+  if keyIsDown
+    
+    % Note that we use find(keyCode) because keyCode is an array.
+    % See 'help KbCheck'
+    counter = counter + 1;
+    test = ['Press the keyboard to advance in the race!'];
+    
+    % draw the square onto the screen
+    Screen(window, 'TextSize', 42);        % default text font size
+    Screen(window, 'TextFont',  'Arial');  % and
+    DrawFormattedText(window,test,'center',100,[0,200,255]);
+    Screen('FrameRect',window,RectColor,RedCentre);
+    ImTexture=Screen('MakeTexture',window,fifty_pence);
+    Screen('DrawTexture',window,ImTexture,[],scr.imageSize{1}/2);
+    Screen('Flip',window);
+    FillRed = [590 350 200 100];
+    Screen('FillRect',window,RectColor,[RedCentre(1) RedCentre(2)+600-counter ...
+      RedCentre(3) RedCentre(4)]);
+
+  if counter ==30 
+    
+  DrawFormattedText(window,'not bad, keep going!','center',100,[0,200,255]);
+  Screen('FrameRect',window,RectColor,RedCentre);    
+    ImTexture=Screen('MakeTexture',window,fifty_pence);
+    Screen('DrawTexture',window,ImTexture,[],[]);
+    Screen('Flip',window);
+    FillRed = [590 350 200 100];
+    Screen('FillRect',window,RectColor,[RedCentre(1) RedCentre(2)+600-counter ...
+    RedCentre(3) RedCentre(4)]);
+
+  end
+    if keyCode == escapeKey
+      break;
+    end
+    
+    % If the user holds down a key, KbCheck will report multiple events.
+    % To condense multiple 'keyDown' events into a single event, we wait until all
+    % keys have been released.
+    KbReleaseWait;
+  end
+  
+end
+
+return
+
